b822dc51ee3db3632162c24a92e2c39ffb7e74a8
kannankkking <nagappanramu44@gmail.com>
2026-08-31 11:08:19 +0530

    Fix React component naming for deployment

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./Components/Home";
-import Software_development from "./Components/Software_development";
-import Website_development from "./Components/Website_development";
-import Mobileapp_development from "./Components/Mobileapp_development";
+import SoftwareDevelopment from "./Components/Software_development";
+import WebsiteDevelopment from "./Components/Website_development";
+import MobileAppDevelopment from "./Components/Mobileapp_development";
 import Ecommerce from "./Components/Ecommerce";
 import Marketing from "./Components/Marketing";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Uiux from "./Components/uiux";
 
-
-
-
 function App() {
   return (
-    <div>
-<BrowserRouter>
-<Routes>
-  <Route path="/" element={<Home />}/>
-  <Route path="/About" element={<About />}/>
-  <Route path="/SD" element={<Software_development />}/>
-  <Route path="/WD" element={<Website_development/>}/>
-  <Route path="/MD" element={<Mobileapp_development/>}/>
-  <Route path="/Marketing" element={<Marketing/>}/>
-  <Route path="/Ecommerce" element={<Ecommerce/>}/>
-  <Route path="/contact" element={<Contact/>}/>
-  <Route path="/uiux" element={<Uiux />} />
-</Routes>
-</BrowserRouter>
-   
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/SD" element={<SoftwareDevelopment />} />
+        <Route path="/WD" element={<WebsiteDevelopment />} />
+        <Route path="/MD" element={<MobileAppDevelopment />} />
+        <Route path="/Marketing" element={<Marketing />} />
+        <Route path="/Ecommerce" element={<Ecommerce />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/uiux" element={<Uiux />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
